7e837d1938710eaf7f717d589045d99a704417fd
PowerSlime <powerslime@ya.ru>
2018-04-12 20:10:54 +0300

    Added skills and interests

---
+++ show-magic.js
@@ -0,0 +1,23 @@
+$('.magic').on('click', '.image-container', function() {
+	var image = $(this).closest('.item-container');
+	var paragraph = image.find('p')
+	var old_text = paragraph.text();
+	var new_text = paragraph.data('custom-text');
+
+	if (image.hasClass('magic_clicked')) {
+		image.removeClass('magic_clicked');
+		image.find('img').animate(
+			{opacity: 0},
+			200
+		);
+	} else {
+		image.addClass('magic_clicked');
+		image.find('img').animate(
+			{opacity: 1},
+			200
+		);
+	}
+
+	paragraph.text(new_text);
+	paragraph.data('custom-text', old_text);
+});
